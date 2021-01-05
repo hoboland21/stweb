1f74f2bec382d1a3966a293aaf3dd00dca7f55c4
JC Clark <jc@saipantech.com>
2021-01-05 01:19:08 -0800

    first

---
+++ project-data/angular/startup.sh
@@ -0,0 +1,7 @@
+if [ ! -d stweb ] ; then 
+ng new stweb --defaults=true
+fi
+cd stweb
+npm install
+#ng build --prod --output-path /usr/src/app/django/survey/static/ang/main  --watch --output-hashing none
+ng serve --host 0.0.0.0
---
+++ project-data/django/startup.sh
@@ -0,0 +1,9 @@
+
+if [ ! -d "stweb" ] ;then
+/usr/local/bin/python /usr/local/bin/django-admin  startproject stweb
+fi
+cd stweb
+./manage.py makemigrations --noinput
+./manage.py  migrate
+
+./manage.py runserver 0.0.0.0:8000
