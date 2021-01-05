beff0c54d544c06aee5313af95ec54bd39332250
JC Clark <jc@saipantech.com>
2021-01-05 08:21:35 -0800

    working shell

--- project-data/django/stweb/stweb/settings.py
+++ project-data/django/stweb/stweb/settings.py
@@ -74,10 +74,14 @@ WSGI_APPLICATION = 'stweb.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default' : {
+    'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'stweb',
+        'USER': 'stwebuser',
+        'PASSWORD': 'stwebpass',
+        'HOST': 'mysql',
+        'PORT': '3306',
+    },
 }
 
 
