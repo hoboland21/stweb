3492ef7cd5256defe4b866e196ce3ec92f49e099
JC Clark <jc@saipantech.com>
2021-01-19 22:25:29 -0800

    fix

--- project-data/django/stweb/stweb/settings.py
+++ project-data/django/stweb/stweb/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import datetime 
+from corsheaders.defaults import default_headers
 
+CORS_ALLOW_HEADERS = list(default_headers)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
